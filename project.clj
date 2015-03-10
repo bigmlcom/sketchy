376dae6136c73dde2353646c75a8e0bef07a51a7
Adam Ashenfelter <ashenfad@gmail.com>
2015-03-09 18:31:05 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bigml/sketchy "0.3.0-SNAPSHOT"
+(defproject bigml/sketchy "0.3.0"
   :description "Sketching algorithms in Clojure"
   :url "https://github.com/bigmlcom/sketchy"
   :license {:name "Apache License, Version 2.0"
@@ -7,6 +7,7 @@
             "distcheck" ["do" "clean," "lint," "test"]}
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
+  :jvm-opts ^:replace ["-server"]
   :profiles {:dev {:plugins [[jonase/eastwood "0.1.4"]]}}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [byte-transforms "0.1.3"]])
