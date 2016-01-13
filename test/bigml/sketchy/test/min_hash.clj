e7a629bdc025cfe49e7d47f097e8e2ccdde885e5
Adam Ashenfelter <ashenfad@gmail.com>
2016-01-13 12:35:13 -0800

    Adds a test to confirm faster min-hash performance

--- test/bigml/sketchy/test/min_hash.clj
+++ test/bigml/sketchy/test/min_hash.clj
@@ -1,4 +1,4 @@
-;; Copyright 2013, 2014 BigML
+;; Copyright 2013, 2014, 2015, 2016 BigML
 ;; Licensed under the Apache License, Version 2.0
 ;; http://www.apache.org/licenses/LICENSE-2.0
 
@@ -16,3 +16,12 @@
                                              (mh/into (mh/create)
                                                       (range 500 1500)))
                                    (mh/into (mh/create) (range 1500))))))
+
+(deftest speed-test
+  (let [start (System/currentTimeMillis)
+        similarity
+        (mh/jaccard-similarity (mh/into (mh/create) (range 0.0 1E6))
+                               (mh/into (mh/create) (range 1E5 1E6)))
+        end (System/currentTimeMillis)]
+    (is (< (- end start) 1000))
+    (is (< 0.85 similarity 0.95))))
